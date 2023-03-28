eed97e620b8c446763f27e4554803028eff8a751
kjartanleander <Kjar0321@stud.kea.dk>
2023-03-28 14:45:42 +0200

    Ændret i controller og service

--- src/main/java/com/example/wishlist/Controller/Controller.java
+++ src/main/java/com/example/wishlist/Controller/Controller.java
@@ -1,4 +1,31 @@
 package com.example.wishlist.Controller;
 
+import com.example.wishlist.DTO.WishlistDTO;
+import com.example.wishlist.Service.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@org.springframework.stereotype.Controller
+@RequestMapping(path = "kea")
 public class Controller {
+
+    private Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
+    //View wishlists
+    @GetMapping(path = "/")      //localhost:8082/kea/
+    public String getWishlists (Model model) {
+        List<WishlistDTO> lists = service.getWishlists();
+        model.addAttribute("name", lists);
+        return "index";
+    }
+
+
+
 }
--- src/main/java/com/example/wishlist/Service/Service.java
+++ src/main/java/com/example/wishlist/Service/Service.java
@@ -1,4 +1,23 @@
 package com.example.wishlist.Service;
 
+
+import com.example.wishlist.DTO.WishlistDTO;
+import com.example.wishlist.Repository.Repository;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
 public class Service {
+
+    private Repository repository;
+
+    public Service (Repository repository) {
+        this.repository = repository;
+    }
+
+    public List<WishlistDTO> getWishlists() {
+        return repository.getWishlists();
+    }
+
+
 }
