3546f91126e722b766c8da838fe5f3fc444f0594
tobiasbkreamer <tobias@kraemerne.dk>
2023-04-11 10:16:42 +0200

    Added User and UserDTO

---
+++ src/main/java/com/example/wishlist/DTO/UserDTO.java
@@ -0,0 +1,27 @@
+package com.example.wishlist.DTO;
+
+public class UserDTO {
+    private String email;
+    private String username;
+
+    public UserDTO(String email, String username) {
+        this.email = email;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/com/example/wishlist/Model/User.java
@@ -0,0 +1,27 @@
+package com.example.wishlist.Model;
+
+public class User {
+    private String email;
+    private String username;
+
+    public User(String email, String username) {
+        this.email = email;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
